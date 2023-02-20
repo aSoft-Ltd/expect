685acf4d3019fd0e814697aa3d37a7b53755ccef
andylamax <andylamax@programmer.net>
2023-02-20 14:35:30 +0300

    Monday afternoon sync

--- core/src/jvmTest/java/unit/ExpectationJavaTest.java
+++ core/src/jvmTest/java/unit/ExpectationJavaTest.java
@@ -14,7 +14,6 @@ public class ExpectationJavaTest {
 
     @Test
     public void toBe_of_collection_behave_as_intended() {
-//        expectCollection(List.of(1, 2, 3)).toBeInstanceOf(List.class);
         expectCollection(List.of(4, 5, 6)).toContain(5);
     }
 
